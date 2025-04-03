143898f51b0a9c8242391000dce523f4e4743b4a
fooiskandar <fooiskandar@gmail.com>
2025-04-03 13:51:57 +0700

    update config.ts

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from './types/config'
 export const siteConfig: SiteConfig = {
   title: 'FoO Iskandar WEBID',
   subtitle: 'Personal blog yang Lemah di SEO',
-  lang: 'en','id',       // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+  lang: 'en, id',       // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 245,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
     fixed: false,     // Hide the theme color picker for visitors
