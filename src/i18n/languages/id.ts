491009c50fe36f795e49ee070f8d45d0e2ee3904
fooiskandar <fooiskandar@gmail.com>
2025-04-03 13:22:18 +0700

    update

---
+++ src/i18n/languages/id.ts
@@ -0,0 +1,39 @@
+import Key from '../i18nKey'
+import type { Translation } from '../translation'
+
+export const en: Translation = {
+  [Key.home]: 'Beranda',
+  [Key.about]: 'Tentang',
+  [Key.archive]: 'Arsip',
+  [Key.search]: 'Pencarian',
+  [Key.contact]: 'Kontak',
+
+  [Key.tags]: 'Tag',
+  [Key.categories]: 'Kategori',
+  [Key.recentPosts]: 'Postingan Terbaru',
+
+  [Key.comments]: 'Komen',
+
+  [Key.untitled]: 'Tanpa Judul',
+  [Key.uncategorized]: 'Tidak Berkategori',
+  [Key.noTags]: 'Tanpa Tag',
+
+  [Key.wordCount]: 'kata',
+  [Key.wordsCount]: 'kata-kata',
+  [Key.minuteCount]: 'menit',
+  [Key.minutesCount]: 'menit',
+  [Key.postCount]: 'postingan',
+  [Key.postsCount]: 'postingan',
+
+  [Key.themeColor]: 'Warna Tema',
+
+  [Key.lightMode]: 'Terang',
+  [Key.darkMode]: 'Gelap',
+  [Key.systemMode]: 'Sistem',
+
+  [Key.more]: 'Lebih banyak',
+
+  [Key.author]: 'Pengarang',
+  [Key.publishedAt]: 'Di Publikasikan',
+  [Key.license]: 'Lisensi',
+}
